e9db2318afad71acfb3a61639c7aafdab3b398e4
HelmutSteiner <helmut.steinersti2@gmail.com>
2018-01-13 15:43:18 +0100

    Add new Actor for Cloudinary.

---
+++ src/main/scala/CloudinaryActor.scala
@@ -0,0 +1,17 @@
+import akka.actor.{Actor, ActorLogging}
+
+/**
+  * @author Helmut
+  *
+  *
+  */
+class CloudinaryActor extends Actor with ActorLogging {
+
+  override def receive: Receive = {
+    case PrintMessage(text) =>
+      printMessage(text)
+  }
+
+  private def printMessage(text: String) = log.info(text)
+
+}
