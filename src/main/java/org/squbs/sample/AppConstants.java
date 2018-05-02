9d2b27d5d763b793eb7b2cd539aa4378527add57
Helmut Steiner <helmut.steinersti2@gmail.com>
2018-05-02 09:37:49 +0200

    Update AppConstants.java

--- src/main/java/org/squbs/sample/AppConstants.java
+++ src/main/java/org/squbs/sample/AppConstants.java
@@ -7,6 +7,7 @@ public class AppConstants {
 
     public static final String pingActorPath = "/user/squbs-seed/sample";
 
+    public static final String emptyString  =  "";
     public static final String emptyRequest = "EmptyRequest";
     public static final String emptyResponse = "EmptyResponse";
     public static final String XmlRequest xmlRequest;
