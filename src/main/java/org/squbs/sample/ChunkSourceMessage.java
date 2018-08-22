577fda4b814b4d71cded12c71614a255e4021cad
Helmut Steiner <helmut.steinersti2@gmail.com>
2018-08-22 09:32:42 +0200

    Update ChunkSourceMessage.java

--- src/main/java/org/squbs/sample/ChunkSourceMessage.java
+++ src/main/java/org/squbs/sample/ChunkSourceMessage.java
@@ -8,7 +8,7 @@ import akka.stream.javadsl.Source;
  * be immutable.
  */
 public class ChunkSourceMessage {
-    private Set<> = new HashSet<>();
+    private Set<> = new ConcurtentHashSet<>();
     private List <T> = new ConcurrentHashList<>();
     private Queue<T> = new DelayQueue<T>();
     public final Source<PingResponse, NotUsed> source;
