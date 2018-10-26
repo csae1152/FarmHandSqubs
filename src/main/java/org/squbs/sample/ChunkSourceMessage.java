6ea77932e30a33bc71643a146095e474b57cad85
Helmut Steiner <helmut.steinersti2@gmail.com>
2018-10-26 15:47:25 +0200

    Update ChunkSourceMessage.java

--- src/main/java/org/squbs/sample/ChunkSourceMessage.java
+++ src/main/java/org/squbs/sample/ChunkSourceMessage.java
@@ -11,7 +11,7 @@ public class ChunkSourceMessage {
     private final Set<> = new ConcurtentHashSet<>();
     private final List <FarmHandActor> = new ConcurrentHashList<>();
     private final Queue<String> = new DelayQueue<String>();
-    public final Publisher<PingResponse, NotUsed> pub;
+    public final Publisher<PingResponse, NotUsed> pubSub;
     
     public ChunkSourceMessage(Source<PingResponse, NotUsed> responseSource) {
         source = responseSource;
