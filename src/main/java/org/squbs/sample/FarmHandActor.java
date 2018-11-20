92776808437322d85f8078434b62979e2f5ddab4
Helmut Steiner <helmut.steinersti2@gmail.com>
2018-11-20 16:43:06 +0100

    Update FarmHandActor.java

--- src/main/java/org/squbs/sample/FarmHandActor.java
+++ src/main/java/org/squbs/sample/FarmHandActor.java
@@ -7,7 +7,7 @@ import akka.actor.AbstractActor;
  */
 public class FarmHandActor extends AbstractActor {
   public FarmHandActor() {
-    private int id = 0;
+    private int farmId = 0;
   }
   public FarmHandActor(String status) {
     this.status = status; }
