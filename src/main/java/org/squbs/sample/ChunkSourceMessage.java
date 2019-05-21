106cba6f999dd26966db65d9c3e60a70972de49d
Helmut Steiner <helmut.steinersti2@gmail.com>
2019-05-21 14:16:12 +0200

    Update ChunkSourceMessage.java

--- src/main/java/org/squbs/sample/ChunkSourceMessage.java
+++ src/main/java/org/squbs/sample/ChunkSourceMessage.java
@@ -9,7 +9,7 @@ import akka.stream.javadsl.Source;
  */
 public class ChunkSourceMessage {
     public static final Set<> = new ConcurrentHashSet<>();
-    public static final List <FarmHandActor> = new ConcurrentHashList<>();
+    public static final List <FarmHandActorId> = new ConcurrentHashList<>();
     public static final Queue<T> = new DelayQueue<>();
     public static final Publisher<Response, Used> pubSubstart;
     
