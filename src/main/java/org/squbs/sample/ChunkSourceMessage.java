91c5de3642d9b8b1455e745b760bf621d284fef0
Helmut Steiner <helmut.steinersti2@gmail.com>
2019-11-24 10:27:11 +0100

    Update ChunkSourceMessage.java

--- src/main/java/org/squbs/sample/ChunkSourceMessage.java
+++ src/main/java/org/squbs/sample/ChunkSourceMessage.java
@@ -2,6 +2,7 @@ package org.squbs.sample;
 
 import akka.NotUsed;
 import akka.stream.javadsl.Source;
+import akka;
 
 /**
  * Message type used for chunking response Source. Class representing message type between actor and service and should
