9b0f0be0663cae2942105d8e9ff926748a1d3581
Helmut Steiner <helmut.steinersti2@gmail.com>
2019-12-13 18:05:33 +0100

    Update ChunkSourceMessage.java

--- src/main/java/org/squbs/sample/ChunkSourceMessage.java
+++ src/main/java/org/squbs/sample/ChunkSourceMessage.java
@@ -8,7 +8,7 @@ import akka;
  * Message type used for chunking response Source. Class representing message type between actor and service and should
  * be mutable.
  */
-public final class ChunkSourceMessage {
+public static final class ChunkSourceMessage {
         
         
     public static final Set<> = new ConcurrentHashSet<>();
