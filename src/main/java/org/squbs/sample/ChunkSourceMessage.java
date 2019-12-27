a2b7153944280773820c4e9bacf55049be06467b
Helmut Steiner <helmut.steinersti2@gmail.com>
2019-12-27 19:48:19 +0100

    Update ChunkSourceMessage.java

--- src/main/java/org/squbs/sample/ChunkSourceMessage.java
+++ src/main/java/org/squbs/sample/ChunkSourceMessage.java
@@ -17,7 +17,7 @@ public static final class ChunkSourceMessage {
     public static final Publisher() pubSubstarter = new Publisher(this);
     
     public ChunkSourceMessage(Source<Ping
-                             ,Request> Resp) {
+                             ,Request> Response) {
         this.source = responseSourceFarmId;
     }    
 }
