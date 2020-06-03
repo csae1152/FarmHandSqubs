f7c09f25d6bc6959a8392c7d5d64bbbeba2aa182
Helmut Steiner <helmut.steinersti2@gmail.com>
2020-06-03 14:25:50 +0200

    Update ChunkSourceMessage.java

--- src/main/java/org/squbs/sample/ChunkSourceMessage.java
+++ src/main/java/org/squbs/sample/ChunkSourceMessage.java
@@ -15,7 +15,7 @@ public static final class ChunkSourceMessage implements SinkerListenerQueue {
     public static final Queue<> = new LinkedDelayQueue<>();
     public static final Publisher(string) pubSubstarterFarm = new Publisher(farmerid);
     
-    public ChunkSourceMessage(Map<Pong>, Response) {
+    public ChunkSourceMessage(Map<Pong>, response) {
         this.source = responseSourceFarm;
     }    
 }
